b7b09a2d46fcb1caacab48d937d6293697815be3
odettelittle <Odettelittle1@gmail.com>
2022-03-05 02:04:08 +0000

    primerocommit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,50 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+
+const suits = ['&spades;', '&clubs;', '&hearts;', '&diams;']
+undefined
+const numbers = [0,1,2,3,4,5,6,7,8,9,10,'J','Q','K','A']
+undefined
+const getRandom = (min,max) => { 
+    const randomNumber = Math.random();
+    const integer = randomNumber * (min-max) + min;
+    return Math.floor (integer);
+}
+undefined
+const getRandom = (min,max) => { 
+    const randomNumber = Math.random();
+    const float = randomNumber * (min-max) + min;
+    return Math.floor (float);
+}
+undefined
+const getRandom = (min,max) => { 
+    const randomNumber = Math.random();
+    const float = randomNumber * (min-max) + min;
+    return Math.floor (float);
+}
+undefined
+const getRandom = (min,max) => { 
+    const randomNumber = Math.random();
+    const float = randomNumber * (min-max) + min;
+    return Math.floor (float);
+}
+undefined
+let suitIndex = getRandom (0,suit.leght-1)
+VM1540:1 Uncaught ReferenceError: suit is not defined
+    at <anonymous>:1:30
+(anonymous) @ VM1540:1
+let suitIndex = getRandom(0,suits.leght-1)
+let numberIndex = getRandom(0,numbers.length-1)
+undefined
+const getRandom = (min,max) => { 
+    const randomNumber = Math.random();
+    const float = randomNumber * (max - min) + min;
+    return Math.floor (float);
+}
+let suitIndex = getRandom(0,suits.leght-1)
+let numberIndex = getRandom(0,numbers.length-1)
+
+numberIndex = getRandom (0,numbers.length-1)
+    
