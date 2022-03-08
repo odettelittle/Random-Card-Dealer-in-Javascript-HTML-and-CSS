e55c3445ef0cb5c451d428467146e04c2cd27e16
odettelittle <Odettelittle1@gmail.com>
2022-03-08 01:52:13 +0000

    cuartocommit

--- src/app.js
+++ src/app.js
@@ -5,29 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
-
 const suits = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
 const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 
 const getRandom = (min, max) => {
   const randomNumber = Math.random();
-  const integer = randomNumber * (min - max) + min;
+  const integer = randomNumber * (max - min) + min;
   return Math.floor(integer);
 };
 
 let suitsIndex = getRandom(0, suits.length - 1);
-let numberIndex = getRandom(0, numbers.length - 1);
+let numbersIndex = getRandom(0, numbers.length - 1);
 
+// const body = document.querySelector("body");
+const suitTop = document.querySelector(".suit.top");
+const numberContainer = document.querySelector(".numbers");
+const suitBottom = document.querySelector(".suit.bottom");
 
-const body = document.querySelector("body")
-const suitTop = body.queryselector(".suit.top")
-const numberContainer = body.document.querySelector(".number")
-const suitBottom = body.querySelector(".suit.bottom")
-
-numberContainer.innerHtml = numberIndex;
-suitTop.innerHtml = suitsIndex ;
-suitBottom.innerHtml = suitsIndex;
+debugger;
+window.onload = function() {
+  numberContainer.innerHTML = numbers[numbersIndex];
+  suitTop.innerHTML = suits[suitsIndex];
+  suitBottom.innerHTML = suits[suitsIndex];
+};
